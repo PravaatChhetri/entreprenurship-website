cc4d6f2606af5eade1a4666b791ed2b1f53f3514
popss <pemalw99@gmail.com>
2023-06-17 00:13:43 +0600

    home page

---
+++ src/components/Counter.js
@@ -0,0 +1,130 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import axios from "axios";
+
+const apitoken =
+	"a011c6bc3920f5046b16031c19216beba64cca2f4815f1d225e44e7601646b1e00c7d76b6dc0a15fc43da74a8b7619efcbeaaa0bb2a525983ac43c43580a03fc7423112c6462c902049b516f484e78c4eef140969f14ccc1be970885872619e120579a2d8cba9cf1754f7571ec8c407f8dedbd8f7454560747635f3020efae7e";
+
+const Counter = () => {
+	const becGraduatesRef = useRef(null);
+	const aecGraduatesRef = useRef(null);
+	const totGraduatesRef = useRef(null);
+	const eventsRef = useRef(null);
+	const incubatesRef = useRef(null);
+	const startupsRef = useRef(null);
+
+
+	var bec = 35;
+	var aec = 21;
+	var tot = 31;
+	var eve = 20;
+	var incub = 6;
+	var sups = 35;
+
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:1337/api/stats", {
+				headers: { Authorization: `Bearer ${apitoken}` },
+			})
+			.then((response) => {
+				setData(response.data.data);
+				bec = data[0].attributes.becGraduates;
+				aec = data[0].attributes.aecGraduates;
+				tot = data[0].attributes.totGraduates;
+				eve = data[0].attributes.events;
+				incub = data[0].attributes.incubates;
+				sups = data[0].attributes.startups;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	const { ref, inView } = useInView({
+		threshold: 0.5,
+		triggerOnce: true,
+	});
+
+	useEffect(() => {
+		if (inView && data.length > 0) {
+			animateValue(becGraduatesRef.current, 0, data[0].attributes.becGraduates, 1500);
+			animateValue(aecGraduatesRef.current, 0, data[0].attributes.aecGraduates, 2000);
+			animateValue(totGraduatesRef.current, 0, data[0].attributes.totGraduates, 1200);
+			animateValue(eventsRef.current, 0, data[0].attributes.events, 2000);
+			animateValue(incubatesRef.current, 0, data[0].attributes.incubates, 2000);
+			animateValue(startupsRef.current, 0, data[0].attributes.startups, 2000);
+		}
+	}, [inView]);
+
+	const animateValue = (element, start, end, duration) => {
+		let startTimestamp = null;
+		const step = (timestamp) => {
+			if (!startTimestamp) startTimestamp = timestamp;
+			const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+			const value = Math.floor(progress * (end - start) + start);
+			element.innerHTML = value;
+			if (progress < 1) {
+				window.requestAnimationFrame(step);
+			}
+		};
+		window.requestAnimationFrame(step);
+	};
+
+	return (
+		<div
+			ref={ref}
+			className="flex items-center justify-around py-8 px-8 text-white bg-blue-500"
+		>
+			<div className="flex flex-col items-center justify-center">
+				<div ref={becGraduatesRef} className="text-4xl font-bold text-white counter">
+					{bec}
+				</div>
+				<div className="text-lg">BEC Graduates</div>
+			</div>
+			<hr className="h-24 mx-6 border-l border-white" />
+			
+			<div className="flex flex-col items-center justify-center">
+				<div ref={aecGraduatesRef} className="text-4xl font-bold text-white counter">
+					{aec}
+				</div>
+				<div className="text-lg">AEC Graduates</div>
+			</div>
+			<hr className="h-24 mx-6 border-l border-white" />
+
+			<div className="flex flex-col items-center justify-center">
+				<div ref={totGraduatesRef} className="text-4xl font-bold text-white counter">
+					{tot}
+				</div>
+				<div className="text-lg">TOT Graduates</div>
+			</div>
+			<hr className="h-24 mx-6 border-l border-white" />
+
+			<div className="flex flex-col items-center justify-center">
+				<div ref={eventsRef} className="text-4xl font-bold text-white counter">
+					{eve}
+				</div>
+				<div className="text-lg">Events</div>
+			</div>
+			<hr className="h-24 mx-6 border-l border-white" />
+
+			<div className="flex flex-col items-center justify-center">
+				<div ref={incubatesRef} className="text-4xl font-bold text-white counter">
+					{incub}
+				</div>
+				<div className="text-lg">Incubates</div>
+			</div>
+			<hr className="h-24 mx-6 border-l border-white" />
+
+			<div className="flex flex-col items-center justify-center">
+				<div ref={startupsRef} className="text-4xl font-bold text-white counter">
+					{sups}
+				</div>
+				<div className="text-lg">Start-Ups</div>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,185 @@
 import React from 'react'
+import Counter from "../components/Counter.js"
+import herobg1 from "../assets/images/hero-bg1.jpg"
+import herobg2 from "../assets/images/hero-bg2.jpg"
+import herobg3 from "../assets/images/hero-bg3.jpg"
+import herobg4 from "../assets/images/hero-bg4.jpg"
+import user1 from "../assets/images/user.jpg"
+import event1 from "../assets/images/hackathon.jpg"
+import {CiCircleChevRight, CiCircleChevLeft} from "react-icons/ci"
 
 const Home = () => {
   return (
-    <div><p>Home</p></div>
+    // hero section
+    <div className="bg-slate-200">
+      <img src={herobg1} alt="Hero Background" className="w-full h-[400px]"/>
+
+
+      {/* Announcement section */}
+      <div>
+        <h1 className="text-[35px] font-bold text-center mt-8 text-[#1d83bd]">
+          Announcements
+        </h1>
+
+        <div className="flex justify-center">
+          <div className="flex flex-col mt-8">
+            {/* Announcement items */}
+            <div className="flex flex-col lg:flex-row mb-6">
+              <div className="bg-blue-500 w-[120px] h-[120px] pt-3 rounded-lg text-center">
+                <p className="text-6xl">15</p>
+                <p className="font-bold">June 2023</p>
+              </div>
+              <div className="pt-6 pl-4">
+                <p className="font-bold">Bhutan Startup Hackathon Challenge</p>
+                <p>Empowering Start-up Ecosystem through Digital Transformation..Read more</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row mb-6">
+              <div className="bg-blue-500 w-[120px] h-[120px] pt-3 rounded-lg text-center">
+                <p className="text-6xl">05</p>
+                <p className="font-bold">March 2023</p>
+              </div>
+              <div className="pt-6 pl-4">
+                <p className="font-bold">Bhutan Startup Hackathon Challenge</p>
+                <p>Empowering Start-up Ecosystem through Digital Transformation..Read more</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col lg:flex-row mb-6">
+              <div className="bg-blue-500 w-[120px] h-[120px] pt-3 rounded-lg text-center">
+                <p className="text-6xl">05</p>
+                <p className="font-bold">March 2023</p>
+              </div>
+              <div className="pt-6 pl-4">
+                <p className="font-bold">Bhutan Startup Hackathon Challenge</p>
+                <p>Empowering Start-up Ecosystem through Digital Transformation..Read more</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      {/* Events section */}
+      <div className="bg-white pt-10 pb-20">
+        <h1 className="text-[35px] font-bold text-center text-[#1d83bd]">
+          Events
+        </h1>
+
+        {/* Event items */}
+        <div className="flex justify-center flex-row row-span-3 gap-16 mt-10">
+        <CiCircleChevLeft className="w-10 h-10"/>
+          {/* Event 1 */}
+          <div className="card w-80 h-[450px] bg-slate-100 shadow-lg">
+            <figure className="px-10 pt-10">
+              <img src={event1} className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title pt-5 font-bold text-xl">Bhutan Startup Hackathon Challenge</h2>
+              <p className="text-xs pt-3">Empowering Start-up Ecosystem through Digital Transformation</p>
+              <div className="card-actions">
+                <button className="btn btn-primary p-2 mt-3 border-2 border-blue-500 rounded-lg font-semibold hover:bg-blue-500 hover:text-white">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Event 2 */}
+          <div className="card w-80 h-[450px] bg-slate-100 shadow-lg">
+            <figure className="px-10 pt-10">
+              <img src={event1} className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title pt-5 font-bold text-xl">Bhutan Startup Hackathon Challenge</h2>
+              <p className="text-xs pt-3">Empowering Start-up Ecosystem through Digital Transformation</p>
+              <div className="card-actions">
+                <button className="btn btn-primary p-2 mt-3 border-2 border-blue-500 rounded-lg font-semibold hover:bg-blue-500 hover:text-white">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Event 3 */}
+          <div className="card w-80 h-[450px] bg-slate-100 shadow-lg">
+            <figure className="px-10 pt-10">
+              <img src={event1} className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title pt-5 font-bold text-xl">Bhutan Startup Hackathon Challenge</h2>
+              <p className="text-xs pt-3">Empowering Start-up Ecosystem through Digital Transformation</p>
+              <div className="card-actions">
+                <button className="btn btn-primary p-2 mt-3 border-2 border-blue-500 rounded-lg font-semibold hover:bg-blue-500 hover:text-white">Read More</button>
+              </div>
+            </div>
+          </div>
+          <CiCircleChevRight className="w-10 h-10"/>
+        </div>
+      </div>
+
+      {/* counter */}
+      <Counter/>
+
+      {/* testimonials */}
+      <div>
+        <h1 className="text-[35px] font-bold text-center mt-8 text-slate-600">
+          Testimonials
+        </h1>
+
+        <div className="flex flex-row row-span-2 gap-20 py-14 justify-center">
+          {/* testimonial 1 */}
+          <div className="card w-80 h-[450px] bg-slate-100 border-4 border-t-blue-500 rounded-3xl">
+              <div className="mx-[35%] my-10 w-28 rounded-full">
+                <img src={user1} className="rounded-full" />
+              </div>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title font-bold text-xl text-blue-500">Pravaat</h2>
+                <p className="p-5 text-justify">"Thanks to Startup Bhutan, my business has experienced exponential growth. Highly recommended!"</p>
+              </div>
+          </div>
+
+          {/* testimonial 2 */}
+          <div className="card w-80 h-[450px] bg-slate-100 border-4 border-t-blue-500 rounded-3xl">
+              <div className="mx-[35%] my-10 w-28 rounded-full">
+                <img src={user1} className="rounded-full" />
+              </div>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title font-bold text-xl text-blue-500">Dechen</h2>
+                <p className="p-5 text-justify">"Startup Bhutan has been a game-changer for our startup. Their network, resources, and guidance have helped us scale our business and reach new heights."</p>
+              </div>
+          </div>
+
+        </div>
+      </div>
+
+      {/* Booking Incubation space */}
+      <div className="flex flex-row row-span-2 h-[550px]">
+        <div className="bg-blue-500 w-[30%]">
+          <h1 className="text-center font-bold text-4xl text-white pt-8 px-24">Book Your Incubation Space Here!</h1>
+        </div>
+        {/* form */}
+        <div className="bg-white w-[70%]">
+          <h1 className="font-bold text-2xl my-8 text-center">Registration Form</h1>
+          <div className="flex flex-row row-span-2 gap-20 justify-center py-10">
+            <div className="flex flex-col gap-6">
+              <input type="text" placeholder="Enter your name here" className="p-2 bg-slate-200 rounded-lg"/>
+              <input type="text" placeholder="Company Name" className="p-2 bg-slate-200 rounded-lg"/>
+              <input type="email" placeholder="Email" className="p-2 bg-slate-200 rounded-lg"/>
+              <input type="text" placeholder="Contact Number" className="p-2 bg-slate-200 rounded-lg"/>
+            </div>
+            <div className="flex flex-col gap-6">
+            <input type="text" placeholder="Description/Remarks" className="px-2 h-24 bg-slate-200 rounded-lg"/>
+            <input type="date" placeholder="Start Date" className="p-2 bg-slate-200 rounded-lg"/>
+            <input type="date" placeholder="End Date" className="p-2 bg-slate-200 rounded-lg"/>
+            </div>
+          </div>
+          <div className="flex justify-center my-8">
+            <button className="p-2 w-32 bg-blue-500 rounded-md text-center font-semibold text-white hover:border-2 hover:border-blue-500 hover:bg-transparent hover:text-blue-500">Submit</button>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
