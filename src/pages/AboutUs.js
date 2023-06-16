aaef6a31ec734e10b813dc913a5100e77ec8bbd5
KezangLoday <02200150.cst@rub.edu.bt>
2023-06-17 00:41:53 +0600

    about us update

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,32 +1,252 @@
 import React from "react";
 import HeaderImg from "../img/startupBT.PNG";
+import Dhi from "../img/dhi.webp";
+import loden from "../img/loden.png";
 
 const AboutUs = () => {
   return (
-    <div>
+    <div className="mb-10">
       <div className="">
-        <div className="relative flex justify-center items-start h-[400px] w-auto">
+        <div className="relative flex justify-center items-start h-[400px] w-auto" >
           <img
             src={HeaderImg}
             alt="Startup Background"
-            className="w-full lg:h-900 relative-z-30"
+            className="w-full lg:h-full relative object-bottom object-cover"
           />
           <h1 className="absolute bottom-40 lg:bottom-10 left-0 px-6 py-10 font-bold text-white md:px-20 md:py-10 md:text-8xl">
             About Us
           </h1>
-        </div>  
+        </div>
+      </div>
+      I
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Our Vision</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+              To create a world where every individual with an entrepreneurial
+              spirit has the opportunity to turn their ideas into successful and
+              impactful ventures, contributing to economic growth and societal
+              progress.{" "}
+            </p>
+          </div>
+        </div>
       </div>
       <div className="relative container mx-auto mt-8">
-          <h2 className="text-2xl font-bold my-4">Our Vision</h2>
-          <div className="flex justify-center">
-            <div className="max-w-xl w-full bg-white shadow-md rounded-lg p-6">
-              <p className="text-gray-700">
-              To create a world where every individual with an entrepreneurial spirit has 
-              the opportunity to turn their ideas into successful and impactful ventures, 
-              contributing to economic growth and societal progress.              </p>
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Our Mission</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+              <ul className="list-disc list-inside flex flex-col gap-y-[15px]">
+                <li>
+                  <span className="font-bold">Empowering Startups:</span> Our
+                  mission is to empower startups by providing the resources,
+                  mentorship, and support they need to thrive and make a
+                  positive impact.
+                </li>
+                <li>
+                  <span className="font-bold">Fostering Mentorship:</span> We
+                  foster a community of experienced mentors who share their
+                  knowledge and guidance to accelerate the growth of startups.
+                </li>
+                <li>
+                  <span className="font-bold">Facilitating Collaboration:</span>{" "}
+                  We facilitate networking and collaboration opportunities among
+                  startups, mentors, investors, and industry experts.
+                </li>
+                <li>
+                  <span className="font-bold">Driving Innovation:</span> We
+                  drive innovation by encouraging the development and adoption
+                  of cutting-edge technologies and disruptive ideas.
+                </li>
+                <li>
+                  <span className="font-bold">Promoting Diversity:</span> We
+                  promote diversity and inclusion, creating an environment that
+                  welcomes entrepreneurs from all backgrounds.
+                </li>
+                <li>
+                  <span className="font-bold">Enabling Funding Access:</span> We
+                  enable startups to access funding opportunities by connecting
+                  them with potential investors and providing fundraising
+                  guidance.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Advocating for Entrepreneurship:
+                  </span>{" "}
+                  We advocate for entrepreneurship, promoting policies and
+                  initiatives that support the growth of startups.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Policy and Regulations</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+              <ul className="list-disc list-inside flex flex-col gap-y-[15px]">
+                <li>
+                  <span className="font-bold">Confidentiality Policy:</span> We
+                  maintain strict confidentiality of all sensitive information
+                  shared by startups, mentors, and other stakeholders.
+                </li>
+                <li>
+                  <span className="font-bold">Code of Conduct:</span> We uphold
+                  a code of conduct that promotes professionalism, respect, and
+                  ethical behavior among all participants in our community.
+                </li>
+                <li>
+                  <span className="font-bold">Data Privacy Policy:</span> We are
+                  committed to protecting the privacy and security of personal
+                  data collected and processed through our platform.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Intellectual Property Policy:
+                  </span>{" "}
+                  We respect and protect the intellectual property rights of
+                  startups and mentors, fostering an environment that encourages
+                  innovation and creativity.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Non-Disclosure Agreement (NDA):
+                  </span>{" "}
+                  We offer a standard Non-Disclosure Agreement for startups and
+                  mentors to safeguard their confidential information during
+                  engagements and collaborations.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Compliance with Laws and Regulations:
+                  </span>{" "}
+                  We adhere to all applicable laws, regulations, and industry
+                  standards to ensure the legality and integrity of our
+                  operations.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Framework</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+              At Startup Bhutan, we follow a robust framework that combines
+              industry best practices and innovative methodologies. Our
+              framework encompasses strategic planning, agile development,
+              continuous improvement, and stakeholder engagement. By leveraging
+              this framework, we ensure the efficient and effective delivery of
+              our services and support to startups and mentors.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Mandates</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+              <ul className="list-disc list-inside flex flex-col gap-y-[15px]">
+                <li>
+                  <span className="font-bold">Empowering Entrepreneurs:</span>{" "}
+                  We are dedicated to empowering entrepreneurs by providing them
+                  with the necessary resources, guidance, and support to succeed
+                  in their ventures.
+                </li>
+                <li>
+                  <span className="font-bold">Fostering Collaboration:</span> We
+                  believe in the power of collaboration and actively facilitate
+                  connections among startups, mentors, investors, and industry
+                  experts to foster a thriving entrepreneurial community.
+                </li>
+                <li>
+                  <span className="font-bold">Promoting Innovation:</span> We
+                  encourage and support innovative ideas and technologies that
+                  have the potential to create positive change and drive
+                  economic growth.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Embracing Diversity and Inclusion:
+                  </span>{" "}
+                  We value diversity and inclusivity and strive to create an
+                  environment where individuals from all backgrounds feel
+                  welcome and have equal opportunities.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Advocating for Supportive Policies:
+                  </span>{" "}
+                  We advocate for policies and regulations that create a
+                  favorable ecosystem for startups, enabling them to thrive and
+                  make a meaningful impact on the economy and society.
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Promoting Sustainable Practices:
+                  </span>{" "}
+                  We promote sustainable business practices among startups,
+                  encouraging them to consider social and environmental impacts
+                  while driving economic growth.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">DoEE, EPD</h2>
+        <div className="flex justify-left">
+          <div className="min-w-xl max-w-[80vw] bg-white shadow-md rounded-lg p-6 mx-auto">
+            <p className="text-gray-700 text-xl">
+            The Department of Employment and Entrepreneurship (DoEE) is a government organization dedicated to fostering 
+            entrepreneurship and promoting employment opportunities. With a strong focus on supporting startups and small 
+            businesses, DoEE provides valuable resources, mentorship, and guidance to aspiring entrepreneurs. By facilitating 
+            access to funding, offering business development programs, and creating an enabling ecosystem, DoEE plays a crucial 
+            role in nurturing a thriving entrepreneurial landscape.
+            {" "}
+            </p>
+            <br/>
+            <p className="text-gray-700 text-xl">
+            The Entrepreneurship Promotion Division (EPD) is a key division within DoEE, responsible for driving initiatives that 
+            encourage and support entrepreneurship. EPD focuses on creating an ecosystem conducive to startup growth, fostering 
+            innovation, and providing platforms for knowledge sharing and collaboration. Through targeted programs, events, and 
+            partnerships, EPD plays a vital role in connecting entrepreneurs with mentors, investors, and industry experts, enabling 
+            them to realize their entrepreneurial aspirations and contribute to economic development.
+            {" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="relative container mx-auto mt-8">
+        <h2 className="text-2xl font-bold my-4 text-[#02395c]">Our Partners</h2>
+        <div className="flex justify-left">
+          <div className="flex flex-nowrap overflow-x-auto">
+            <div className="min-w-xl w-[calc(50vw - 1rem)] bg-white shadow-md rounded-lg p-6 mx-2">
+              <img
+                src={Dhi}
+                alt="DHI"
+                className="w-full h-auto object-contain rounded-lg"
+                style={{ maxWidth: "200px", maxHeight: "200px" }}
+              />
+            </div>
+            <div className="min-w-xl w-[calc(50vw - 1rem)] bg-white shadow-md rounded-lg p-6 mx-2">
+              <img
+                src={loden}
+                alt="loden foundation"
+                className="w-full h-auto object-contain rounded-lg"
+                style={{ maxWidth: "200px", maxHeight: "200px" }}
+              />
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
