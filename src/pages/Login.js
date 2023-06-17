b447854fe83360e5f6657d2386b223d4012ceb0f
popss <pemalw99@gmail.com>
2023-06-17 11:15:55 +0600

    login and signin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,12 @@ import React, { useState } from 'react';
 
 const Login = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    cid: '',
     email: '',
-    mobile: '',
-    gender: '',
     status: '',
-    dob: '',
     password: '',
-    termsAccepted: false,
   });
 
-  const { name, cid, email, mobile, gender, status, dob, password, termsAccepted } = formData;
+  const { email, status, password } = formData;
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -33,7 +27,7 @@ const Login = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Verification logic goes here
-    if (!name || !cid || !email || !mobile || !gender || !status || !dob || !password || !termsAccepted) {
+    if (!email || !status || !password) {
       alert('Please fill in all the fields');
     } else {
       // Form submission logic goes here
@@ -43,37 +37,11 @@ const Login = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md w-[500px] max-w-md border-2 border-[#FFA500]">
-        <h2 className="text-3xl font-semibold mb-4">Registration Form</h2>
+      <div className="bg-white p-8 rounded-xl w-[500px] max-w-md border-2 shadow-2xl my-10 border-orange-500">
+        <h2 className="text-3xl font-semibold mb-4 text-center text-[#808080]">Registration Form</h2>
         <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name" className="text-gray-700">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
-              placeholder="Enter your name"
-            />
-          </div>
-          <div>
-            <label htmlFor="cid" className="text-gray-700">
-              CID/Passport Number
-            </label>
-            <input
-              type="text"
-              id="cid"
-              name="cid"
-              value={cid}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
-              placeholder="Enter your CID/Passport number"
-            />
-          </div>
+          
+          
           <div>
             <label htmlFor="email" className="text-gray-700">
               Email ID
@@ -84,69 +52,27 @@ const Login = () => {
               name="email"
               value={email}
               onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-orange-500"
               placeholder="Enter your email address"
             />
           </div>
-          <div>
-            <label htmlFor="mobile" className="text-gray-700">
-              Mobile No
-            </label>
-            <input
-              type="tel"
-              id="mobile"
-              name="mobile"
-              value={mobile}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
-              placeholder="Enter your mobile number"
-            />
-          </div>
-          <div>
-            <label htmlFor="gender" className="text-gray-700">
-              Gender
-            </label>
-            <select
-              id="gender"
-              name="gender"
-              value={gender}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
-            >
-              <option value="">Select your gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
+          
+          
           <div>
             <label htmlFor="status" className="text-gray-700">
-              Status
+              Assigned to
             </label>
             <select
               id="status"
               name="status"
               value={status}
               onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-orange-500"
             >
               <option value="">Select your status</option>
               <option value="mentor">Mentor</option>
               <option value="mentee">Mentee</option>
             </select>
-          </div>
-          <div>
-            <label htmlFor="dob" className="text-gray-700">
-              DOB
-            </label>
-            <input
-              type="date"
-              id="dob"
-              name="dob"
-              value={dob}
-              onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
-            />
           </div>
           <div>
             <label htmlFor="password" className="text-gray-700">
@@ -158,22 +84,9 @@ const Login = () => {
               name="password"
               value={password}
               onChange={handleInputChange}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-orange-500"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-orange-500"
               placeholder="Enter your password"
             />
-          </div>
-          <div className="flex items-center">
-            <input
-              type="checkbox"
-              id="termsAccepted"
-              name="termsAccepted"
-              checked={termsAccepted}
-              onChange={handleTermsCheckboxChange}
-              className="form-checkbox h-4 w-4 text-orange-500 rounded"
-            />
-            <label htmlFor="termsAccepted" className="ml-2 text-gray-700">
-              I accept the terms and policies
-            </label>
           </div>
           <button
             type="submit"
